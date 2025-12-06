cddad1965287fbaf0122d0b0afec3496aaf50f96
Aliaksei Kazlou <lexa.kozllov@gmail.com>
2025-12-06 19:35:57 +0300

    feat: do small changes

--- apps/angular/shell/src/app/app.routes.ts
+++ apps/angular/shell/src/app/app.routes.ts
@@ -3,7 +3,11 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
-    path: '',
+    path: 'shell',
     component: NxWelcome,
+  },
+  {
+    path: 'board',
+    loadChildren: () => import('board/Routes').then((m) => m.remoteRoutes),
   },
 ];
