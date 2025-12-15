051e836368bea7acceddd2c562931870b1059647
Aliaksei Kazlou <lexa.kozllov@gmail.com>
2025-12-15 15:16:49 +0300

    feat: update ci.yml

--- apps/angular/shell/src/app/app.ts
+++ apps/angular/shell/src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { environment } from '../environments/environment';
+import { buildDate, commitHash } from '../environments/version';
 
 @Component({
   imports: [RouterModule],
@@ -7,6 +9,12 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected title = 'shell';
+
+  public environment = environment;
+
+  public ngOnInit(): void {
+    console.log(`MyActivityBoard [shell] build commit: ${commitHash}, built at: ${buildDate}`);
+  }
 }
