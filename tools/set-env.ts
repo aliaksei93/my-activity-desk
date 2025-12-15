6c4f01116d4c08f5ed71a06af4e1fa301c8a41db
Aliaksei Kazlou <lexa.kozllov@gmail.com>
2025-12-15 15:16:49 +0300

    feat: update ci.yml

--- tools/set-env.ts
+++ tools/set-env.ts
@@ -1,21 +1,41 @@
 const { writeFile } = require('fs');
 const path = require('path');
 
-const targetPath = process.env.WEB_APP_ENV_TARGET_PATH;
+const envTargetPath = process.env.WEB_APP_ENV_TARGET_PATH;
+const manifestTargetPath = process.env.WEB_APP_MANIFEST_TARGET_PATH;
 
 const envConfigFile = `
-export const environment = {
-  apiBaseUrl: 'WEB_APP_API_BASE_URL_TO_REPLACE',
-};
-`;
+  export const environment = {
+    apiBaseUrl: 'WEB_APP_API_BASE_URL_TO_REPLACE',
+  };
+  `;
 
-const resolvedPath = path.resolve(targetPath);
+const envResolvedPath = path.resolve(envTargetPath);
 
-writeFile(resolvedPath, envConfigFile, function (err: any) {
+writeFile(envResolvedPath, envConfigFile, function (err: any) {
   if (err) {
     console.log(err);
     process.exit(1);
   }
 
-  console.log(`Output generated at ${targetPath}`);
+  console.log(`environment.ts output generated at ${envTargetPath}`);
 });
+
+if (manifestTargetPath) {
+  const manifestFile = `
+  {
+    "board": "WEB_APP_BOARD_REMOTE_URL_TO_REPLACE/mf-manifest.json"
+  }
+  `;
+
+  const manifestResolvedPath = path.resolve(manifestTargetPath);
+
+  writeFile(manifestResolvedPath, manifestFile, function (err: any) {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+
+    console.log(`module-federation.manifest.json output generated at ${manifestTargetPath}`);
+  });
+}
